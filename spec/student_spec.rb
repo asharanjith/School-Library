60479e988cf4b3a4030cbb6574703e6e56a0c041
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-02-15 16:54:18 +0300

    Add test to the Rental class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,7 +4,7 @@ require_relative '../classroom'
 describe Student do
     context 'Check whether Student class can add new student and create Student instance' do
       it 'When a new student is added, it should be an instance of Student class' do
-        new_student = Student.new(20,'Sam', parent_permission: true,)
+        new_student = Student.new(20,'Sam', parent_permission: true)
         expect(new_student).to be_instance_of(Student)
       end
       it 'Check a student can be added to the classroom' do
